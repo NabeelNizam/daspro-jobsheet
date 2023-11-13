ae115aa6f0bab19447712cc03e5f94977b77b678
NabeelNizam <abee237@gmail.com>
2023-11-13 11:02:13 +0700

    change individu3

--- Jobsheet11/individu3.java
+++ Jobsheet11/individu3.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class individu3 {
     public static void main(String[] args) {
-        
+    Scanner sc = new Scanner (System.in);
+    int angka;
+
+    System.out.print("Masukkan angka: ");
+    angka = sc.nextInt();
+
+    for (int i = 1; i <= angka; i++) {
+        for (int j = 1; j <= angka; j++) {
+            if (i == 1 || i == angka || j == 1 || j == angka) {
+                System.out.print( angka + " ");
+            }else
+            System.out.print("  ");
+        }
+        System.out.println();
+    }
+    sc.close();
     }
 }
